01c427be410c02aec795e08d7d37f6a8a410f0b9
JoeKAboagye <josephkwabenaaboagye@gmail.com>
2022-05-02 16:58:33 +0000

    Using Webpack with a config file

---
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -0,0 +1,8 @@
+import { resolve } from 'path';
+
+export const mode = 'production';
+export const entry = './js/dashboard_main.js';
+export const output = {
+    path: resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+};
